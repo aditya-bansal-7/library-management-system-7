a33b4e69a4e5655373c979b74db8f9bc5ef096df
Aditya Bansal <adityabansalsafe@gmail.com>
2024-11-07 16:53:35 +0530

    feat : created all possible Dao for models

---
+++ src/main/java/com/thesquad/dao/AddressDAO.java
@@ -0,0 +1,121 @@
+package com.thesquad.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.thesquad.models.AddressModel;
+import com.thesquad.connection.DBConnection;
+
+public class AddressDAO {
+
+    public AddressDAO() {
+
+    }
+
+    public void create(AddressModel address, DBConnection connection) {
+        String sql = "INSERT INTO morada(num_casa, rua, bairro, fk_comuna) values(?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, address.getHouseNum());
+            ps.setString(2, address.getStreet());
+            ps.setString(3, address.getDistrict());
+            ps.setInt(4, address.getCommuneId());
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(AddressModel address, DBConnection connection) {
+        String sql = "UPDATE morada SET num_casa = ?, rua = ?, bairro = ?, fk_comuna = ? WHERE pk_morada = ?";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, address.getHouseNum());
+            ps.setString(2, address.getStreet());
+            ps.setString(3, address.getDistrict());            
+            ps.setInt(4, address.getCommuneId());
+            ps.setInt(5, address.getAddressId());
+
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int addressId, DBConnection connection) {
+        String sql = "DELETE FROM morada WHERE pk_morada = ?";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, addressId);
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<AddressModel> getAll(DBConnection connection) {
+        String sql = "SELECT * FROM morada";
+
+        List<AddressModel> addressList = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                AddressModel address = new AddressModel();
+                address.setAddressId(resultSet.getInt(1));
+                address.setStreet(resultSet.getString(2));
+                address.setHouseNum(resultSet.getInt(3));
+                address.setDistrict(resultSet.getString(4));
+                address.setCommuneId(resultSet.getInt(5));
+                address.setCreationDate(resultSet.getTimestamp(6).toLocalDateTime());
+
+                addressList.add(address);
+            }
+            ps.close();
+            resultSet.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return addressList;
+    }
+
+    public AddressModel getAddressById(int addressId, DBConnection connection) {
+        String sql = "SELECT * FROM morada WHERE pk_morada = ?";
+
+        try {
+            AddressModel address = new AddressModel();
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, addressId);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                address.setAddressId(resultSet.getInt(1));
+                address.setStreet(resultSet.getString(2));
+                address.setHouseNum(resultSet.getInt(3));
+                address.setDistrict(resultSet.getString(4));
+                address.setCommuneId(resultSet.getInt(5));
+                address.setCreationDate(resultSet.getTimestamp(6).toLocalDateTime());
+            }
+
+            ps.close();
+            resultSet.close();
+            return address;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/thesquad/dao/BookAuthorDAO.java
@@ -0,0 +1,113 @@
+package com.thesquad.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.thesquad.models.BookAuthorModel;
+import com.thesquad.connection.DBConnection;
+
+public class BookAuthorDAO {
+
+    public BookAuthorDAO() {
+    }
+
+    public void create(BookAuthorModel bookAuthor, DBConnection connection) {
+        String sql = "INSERT INTO livro_autor(fk_livro, fk_autor) values(?, ?)";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, bookAuthor.getBookId());
+            ps.setInt(2, bookAuthor.getAuthorId());
+
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(BookAuthorModel bookAuthor, DBConnection connection) {
+        String sql = "UPDATE livro_autor SET fk_livro = ?, fk_autor = ? WHERE pk_livro_autor = ?";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, bookAuthor.getBookId());
+            ps.setInt(2, bookAuthor.getAuthorId());
+            ps.setInt(3, bookAuthor.getBookAuthorId());
+
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int bookAuthorId, DBConnection connection) {
+        String sql = "DELETE FROM livro_autor WHERE pk_livro_autor = ?";
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, bookAuthorId);
+
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<BookAuthorModel> getAll(DBConnection connection) {
+        String sql = "SELECT * FROM livro_autor";
+
+        List<BookAuthorModel> bookAuthorList = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                BookAuthorModel location = new BookAuthorModel();
+                location.setBookAuthorId(resultSet.getInt(1));
+                location.setBookId(resultSet.getInt(2));
+                location.setAuthorId(resultSet.getInt(3));
+                location.setCreationDate(resultSet.getTimestamp(4).toLocalDateTime());
+
+                bookAuthorList.add(location);
+            }
+            ps.close();
+            resultSet.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return bookAuthorList;
+    }
+
+    public BookAuthorModel getBookAuthorById(int locationId, DBConnection connection) {
+        String sql = "SELECT * FROM livro_autor WHERE pk_livro_autor = ?";
+
+        try {
+            BookAuthorModel location = new BookAuthorModel();
+            PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+            ps.setInt(1, locationId);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                location.setBookAuthorId(resultSet.getInt(1));
+                location.setBookId(resultSet.getInt(2));
+                location.setAuthorId(resultSet.getInt(3));
+                location.setCreationDate(resultSet.getTimestamp(4).toLocalDateTime());
+            }
+
+            ps.close();
+            resultSet.close();
+            return location;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
